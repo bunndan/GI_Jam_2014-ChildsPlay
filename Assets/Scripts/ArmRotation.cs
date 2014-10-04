d2cd0f10bf7c6cd9ddb7fbf2186f0f750750e3d6
Brandon Ma <brandon.ma@live.ca>
2014-10-04 10:09:02 -0400

    Added arm following script.
    When the game runs, the arm now follows the location of the mouse. Used
    for small shooters.

---
+++ Assets/Scripts/ArmRotation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArmRotation : MonoBehaviour {
+
+	public int rotationOffset = 90;
+
+	// Update is called once per frame
+	void Update () {
+		// difference between the position of our mouse in 3D space and the position of our character.
+		Vector3 difference = Camera.main.ScreenToWorldPoint (Input.mousePosition) - transform.position;
+		// Normalizing the 3D vector. Making the sum of the values equal to 1 (x + y + z = 1). Keeping proportions of the values but just making them smaller.
+		difference.Normalize ();
+
+		float rotZ = Mathf.Atan2 (difference.y, difference.x) * Mathf.Rad2Deg; // find the rotation in degrees
+		transform.rotation = Quaternion.Euler (0f, 0f, rotZ + rotationOffset);
+	}
+}
